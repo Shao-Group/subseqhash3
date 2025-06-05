27fb6f403fb857946ca9fdc5e2c9df0718431803
Ajmain Yasar Ahmed <yasar199510@gmail.com>
2025-06-05 17:24:32 -0400

    Hash collision experiment ongoing

--- src/hash_collision.cpp
+++ src/hash_collision.cpp
@@ -3,7 +3,7 @@
 int main(int argc, char** argv) {
     SubseqHash3 subseqhash3(5, 7, {{'A', 0}, {'C', 1}, {'G', 2}, {'T', 3}});
 
-    subseqhash3.generateTables();
+    // subseqhash3.generateTables();
     subseqhash3.loadTables();
 
     subseqhash3.solvePivotDP("ACGTACGTACGTACGT", 10);
