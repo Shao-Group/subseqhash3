31a583d5269033b9233acd362c7bff138dca1e4b
Ajmain Yasar Ahmed <yasar199510@gmail.com>
2025-06-10 18:04:42 -0400

    Hash collision Experiment: Dealing with segmentation fault caused by large arrays and empty seeds

--- src/hash_collision.cpp
+++ src/hash_collision.cpp
@@ -18,13 +18,29 @@ int main(int argc, char** argv) {
         exit(EXIT_FAILURE);
     }
 
+    int numHashCollisions = 0, numTotalSequencePairs = 0;
+
     while(sequencePairsFile >> sequence1) {
         sequencePairsFile >> sequence2;
 
         vector<PiCell> sequence1Seeds = subseqHash3.solvePivotDP(sequence1, sequence1.length()), sequence2Seeds = subseqHash3.solvePivotDP(sequence2, sequence2.length());
+
+        for(int i = 0; i < sequence1Seeds.size(); i++) {
+            // [future task] We should look into why we are getting empty seeds with psi and omega values of d and NEG_INF respectively
+            if(!sequence1Seeds[i].seedData->seed.empty() && !sequence2Seeds[i].seedData->seed.empty()) {
+                if(sequence1Seeds[i].seedData->seed == sequence2Seeds[i].seedData->seed) {
+                    numHashCollisions++;
+                    break;
+                }
+            }
+        }
+
+        numTotalSequencePairs++;
     }
 
     sequencePairsFile.close();
+
+    cout << "Hash collision probability = " << numHashCollisions / numTotalSequencePairs << endl;
     
     return 0;
 }
